4dfba62d6879cc50136b703f94c6d3ef99068dd2
DanielOjo <danielojo@live.com>
2014-09-11 22:35:48 +0100

    Homework stuff

---
+++ Task 6.3.py
@@ -0,0 +1,11 @@
+#Daniel Ogunlana
+#9/9/2014
+#Task 6
+#1.Write a program that will ask the user for three integers and display the total.
+#2.Write a program that will ask the user for two integers and display the result of multiplying them together.
+#3.Ask the user for the length, width and depth of a rectangular swimming pool. Calculate the volume of water required 
+
+#python Task 6.3
+print("Workout the amount of water needed for this swimming pool."
+      "Width 20m Length 50m Depth 5m")
+5000m**3 water needed
---
+++ Test.py
@@ -0,0 +1,11 @@
+![The Python Shell](images/shell.jpg)
+
+|Expression|Symbol description|Answer|
+|:--------:|------------------|------|
+|`3 + 4`|*addition*|*7*|
+|`2 - 1`| | |
+|`5 * 3`| | |
+|`2**3`| | |
+|`27/7`| | |
+|`27//7`| | |
+|`27%7`| | |
